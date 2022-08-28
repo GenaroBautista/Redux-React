46682255a50c202ec0f9943157f294b67ac6ef4c
Genaro Bautista <soporte.gadgethouse@gmail.com>
2022-08-28 15:25:22 -0500

    update redux-react

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css';
+import { faker } from "@faker-js/faker";
+
+function App () {
+  return (
+    <div className="ui container comments">
+      <div className='comments'>
+        <a href='/' className='avatar'>
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className='content '>
+        <a href='/' className='autor'>
+            Sam
+          </a>
+          <div className='metadata'>
+            <span className='date'> Today at 6:00PM</span>
+          </div>
+          <div className='text'> Nice blog post!</div>git 
+         </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
