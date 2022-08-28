39cfbb8f8a5660bcbc722a8e2a3779fd2d562ee1
Genaro Bautista <soporte.gadgethouse@gmail.com>
2022-08-28 16:21:39 -0500

    respaldo

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,23 @@
 import './App.css';
 import { faker } from "@faker-js/faker";
 
-function App () {
+function App() {
   return (
     <div className="ui container comments">
-      <div className='comments'>
-        <a href='/' className='avatar'>
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className='content '>
-        <a href='/' className='autor'>
-            Sam
+      <di className="comments">
+        <a href="/"  className="avatar">
+          <img alt="avatar" src={faker.image.avatar()}/>
           </a>
-          <div className='metadata'>
-            <span className='date'> Today at 6:00PM</span>
+          <div className="content">
+            <a href="/" className={faker.image.avatar}>
+              Sam
+            </a>
+            <div className="metadata">
+              <span className="date">Today at 6:00PM </span>
+            </div>
+            <div className="text">Nice blog post!</div>
           </div>
-          <div className='text'> Nice blog post!</div>git 
-         </div>
-      </div>
+      </di>
     </div>
   );
 }
